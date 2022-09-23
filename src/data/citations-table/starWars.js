616cc5b14007f356aaa0a3cb2c6b04c714470431
Virginie Gaffinet <gaffinet.virginie@gmail.com>
2022-09-23 18:29:50 +0200

    data and assets done

---
+++ src/data/citations-table/starWars.js
@@ -0,0 +1,15 @@
+let starWarsCitations = [
+  ["La peur mène à la colère, la colère mène à la haine… La haine mène à la souffrance !", "Yoda"],
+  ["Quand 900 ans comme moi tu auras, moins en forme tu seras !", "Yoda"],
+  ["Que la Force soit avec toi…", "Obi-Wan"],
+  ["La taille importe peu… Regarde moi : est-ce par ma taille que tu peux me juger ? Et bien tu ne le dois pas…", "Yoda"],
+  ["Tu sais contrôler ta peur… maintenant libère ta colère. Seule ta haine peut me détruire.", "Dark Vador"],
+  ["Je n’aime pas le sable, il est grossier, agressif, irritant et s’insinue partout…", " Anakin"],
+  ["Tu apprendras que beaucoup de vérités auxquelles nous tenons dépendent avant tout de notre propre point de vue.", "Obi-Wan"],
+  ["Je ne viendrai jamais du côté obscur, vous avez échoué votre altesse ! je suis un jedi comme mon père l'avait été avant moi !", "Luke Skywalker"],
+  ["Ainsi s'éteint la liberté, sous une pluie d'applaudissements.", "Padmé Amidala"],
+  ["Si tu refuses de te battre, tu devras affronter ton destin.", "Dark Vador"],
+  ["La boucle est maintenant bouclée. Autrefois j’étais votre apprenti, maintenant je suis le maître.", "Dark Vador"]
+];
+
+export default starWarsCitations;
