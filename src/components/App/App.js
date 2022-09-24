862b42101b671dda54e55c21b0fe0a22ab9aeb33
Virginie Gaffinet <gaffinet.virginie@gmail.com>
2022-09-24 12:00:51 +0200

    header done with css

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,16 @@
-
+// import css
 import './App.css';
+// import data
+import simpsonCitations from '../../data/citations-table/simpson';
+import lordOfTheRingsCitations from '../../data/citations-table/lordOfTheRings';
+import starWarsCitations from '../../data/citations-table/starWars';
+// import composants
+import Header from "../Header/Header";
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
+      <Header />
     </div>
   );
 }
